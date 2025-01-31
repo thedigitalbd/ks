be160c649cc61c6a07cdffcb316f219bd723e933
Ahmed Bin Musa <126979110+thedigitalbd@users.noreply.github.com>
2025-01-31 21:37:35 +0600

    Delete backend directory for new upload

--- backend/generateSecret.js
+++
@@ -1,8 +0,0 @@
-// backend/generateSecret.js
-const crypto = require('crypto');
-
-const generateSecret = () => {
-    return crypto.randomBytes(64).toString('hex'); // Generates a 64-byte random string
-};
-
-console.log(generateSecret());
--- backend/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
-users_bp = Blueprint('users', __name__, url_prefix='/users')
-
-from . import auth, users
